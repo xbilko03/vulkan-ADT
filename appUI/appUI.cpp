12916cdc9e031b99b3de58fa5b2054b1a790a9ea
jozef <jozef@B07-322C>
2023-12-28 22:56:18 +0100

    winsock template fix

--- appUI/appUI.cpp
+++ appUI/appUI.cpp
@@ -28,43 +28,54 @@ static void glfw_error_callback(int error, const char* description)
 // Main code
 int main(int, char**)
 {
-    bool dataTransport = false;
+    bool dataTransport = true;
 
     if (dataTransport)
     {
-        /* Initialize Winsock. */
         WSADATA wsaData;
         int iResult;
+
+        SOCKET ListenSocket = INVALID_SOCKET;
+        SOCKET ClientSocket = INVALID_SOCKET;
+
+        struct addrinfo* result = NULL;
+        struct addrinfo hints;
+
+        int iSendResult;
+        char recvbuf[DEFAULT_BUFLEN];
+        int recvbuflen = DEFAULT_BUFLEN;
 
         // Initialize Winsock
         iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
         if (iResult != 0) {
-            printf("WSAStartup failed: %d\n", iResult);
+            printf("WSAStartup failed with error: %d\n", iResult);
             return 1;
         }
-        /* Create a socket. */
+
         ZeroMemory(&hints, sizeof(hints));
         hints.ai_family = AF_INET;
         hints.ai_socktype = SOCK_STREAM;
         hints.ai_protocol = IPPROTO_TCP;
         hints.ai_flags = AI_PASSIVE;
 
-        // Resolve the local address and port to be used by the server
+        // Resolve the server address and port
         iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
         if (iResult != 0) {
-            printf("getaddrinfo failed: %d\n", iResult);
+            printf("getaddrinfo failed with error: %d\n", iResult);
             WSACleanup();
             return 1;
         }
-        SOCKET ListenSocket = INVALID_SOCKET;
+
+        // Create a SOCKET for the server to listen for client connections.
         ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
         if (ListenSocket == INVALID_SOCKET) {
-            printf("Error at socket(): %ld\n", WSAGetLastError());
+            printf("socket failed with error: %ld\n", WSAGetLastError());
             freeaddrinfo(result);
             WSACleanup();
             return 1;
         }
-        /* Bind the socket. */
+
+        // Setup the TCP listening socket
         iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
         if (iResult == SOCKET_ERROR) {
             printf("bind failed with error: %d\n", WSAGetLastError());
@@ -73,27 +84,28 @@ int main(int, char**)
             WSACleanup();
             return 1;
         }
+
         freeaddrinfo(result);
-        /* Listen on the socket for a client. */
-        if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
-            printf("Listen failed with error: %ld\n", WSAGetLastError());
+
+        iResult = listen(ListenSocket, SOMAXCONN);
+        if (iResult == SOCKET_ERROR) {
+            printf("listen failed with error: %d\n", WSAGetLastError());
             closesocket(ListenSocket);
             WSACleanup();
             return 1;
         }
-        /* Accept a connection from a client. */
-        SOCKET ClientSocket = INVALID_SOCKET;
+
+        // Accept a client socket
         ClientSocket = accept(ListenSocket, NULL, NULL);
         if (ClientSocket == INVALID_SOCKET) {
-            printf("accept failed: %d\n", WSAGetLastError());
+            printf("accept failed with error: %d\n", WSAGetLastError());
             closesocket(ListenSocket);
             WSACleanup();
             return 1;
         }
-        /* Receive and send data. */
-        char recvbuf[DEFAULT_BUFLEN];
-        int iSendResult;
-        int recvbuflen = DEFAULT_BUFLEN;
+
+        // No longer need server socket
+        closesocket(ListenSocket);
 
         // Receive until the peer shuts down the connection
         do {
@@ -105,7 +117,7 @@ int main(int, char**)
                 // Echo the buffer back to the sender
                 iSendResult = send(ClientSocket, recvbuf, iResult, 0);
                 if (iSendResult == SOCKET_ERROR) {
-                    printf("send failed: %d\n", WSAGetLastError());
+                    printf("send failed with error: %d\n", WSAGetLastError());
                     closesocket(ClientSocket);
                     WSACleanup();
                     return 1;
@@ -115,22 +127,24 @@ int main(int, char**)
             else if (iResult == 0)
                 printf("Connection closing...\n");
             else {
-                printf("recv failed: %d\n", WSAGetLastError());
+                printf("recv failed with error: %d\n", WSAGetLastError());
                 closesocket(ClientSocket);
                 WSACleanup();
                 return 1;
             }
 
         } while (iResult > 0);
-        /* Disconnect. */
+
+        // shutdown the connection since we're done
         iResult = shutdown(ClientSocket, SD_SEND);
         if (iResult == SOCKET_ERROR) {
-            printf("shutdown failed: %d\n", WSAGetLastError());
+            printf("shutdown failed with error: %d\n", WSAGetLastError());
             closesocket(ClientSocket);
             WSACleanup();
             return 1;
         }
-        /* Clean */
+
+        // cleanup
         closesocket(ClientSocket);
         WSACleanup();
     }
--- layer/layer.cpp
+++ layer/layer.cpp
@@ -49,13 +49,21 @@ VK_LAYER_EXPORT VkResult VKAPI_CALL SampleLayer_CreateInstance(
         * ptr = NULL,
         hints;
 
+    // Initialize Winsock
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        exit(1);
+    }
+
     ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
     // Resolve the server address and port
-    int iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    iResult = getaddrinfo("localhost", DEFAULT_PORT, &hints, &result);
     if (iResult != 0) {
         printf("getaddrinfo failed: %d\n", iResult);
         WSACleanup();
@@ -66,21 +74,26 @@ VK_LAYER_EXPORT VkResult VKAPI_CALL SampleLayer_CreateInstance(
     // the call to getaddrinfo
     ptr = result;
 
-    // Create a SOCKET for connecting to server
-    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
-        ptr->ai_protocol);
-    if (ConnectSocket == INVALID_SOCKET) {
-        printf("Error at socket(): %ld\n", WSAGetLastError());
-        freeaddrinfo(result);
-        WSACleanup();
-        exit(1);
-    }
-    /* Connect to the server. */
-    // Connect to server.
-    iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
-    if (iResult == SOCKET_ERROR) {
-        closesocket(ConnectSocket);
-        ConnectSocket = INVALID_SOCKET;
+    // Attempt to connect to an address until one succeeds
+    for (ptr = result; ptr != NULL;ptr = ptr->ai_next) {
+
+        // Create a SOCKET for connecting to server
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+            ptr->ai_protocol);
+        if (ConnectSocket == INVALID_SOCKET) {
+            printf("socket failed with error: %ld\n", WSAGetLastError());
+            WSACleanup();
+            exit(1);
+        }
+
+        // Connect to server.
+        iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR) {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
     }
 
     // Should really try the next address returned by getaddrinfo
